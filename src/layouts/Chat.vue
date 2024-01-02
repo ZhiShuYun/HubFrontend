be67ad54ef24fe4b61102075eeec717f5bdbf45a
Germey <cqc@cuiqingcai.com>
2024-01-02 12:12:38 +0800

    mobile adapation for mj

--- src/layouts/Chat.vue
+++ src/layouts/Chat.vue
@@ -3,10 +3,16 @@
     <div class="left">
       <navigator />
     </div>
-    <div class="main">
-      <side-panel />
-      <router-view />
+    <div class="right">
+      <side-panel class="side" />
+      <router-view class="main" />
     </div>
+    <el-button round class="menu" @click="drawer = true">
+      <font-awesome-icon icon="fa-solid fa-bars" class="icon-menu" />
+    </el-button>
+    <el-drawer v-model="drawer" :with-header="false" size="340px" class="drawer">
+      <side-panel />
+    </el-drawer>
   </div>
 </template>
 
@@ -14,12 +20,22 @@
 import { defineComponent } from 'vue';
 import Navigator from '@/components/common/Navigator.vue';
 import SidePanel from '@/components/chat/SidePanel.vue';
+import { ElDrawer, ElButton } from 'element-plus';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 export default defineComponent({
   name: 'LayoutChat',
   components: {
     SidePanel,
-    Navigator
+    Navigator,
+    ElDrawer,
+    ElButton,
+    FontAwesomeIcon
+  },
+  data() {
+    return {
+      drawer: false
+    };
   },
   async mounted() {
     await this.onGetApplications();
@@ -47,7 +63,7 @@ export default defineComponent({
     height: 100%;
     border-right: 1px solid var(--el-border-color);
   }
-  .main {
+  .right {
     height: 100%;
     flex: 1;
     width: calc(100% - 60px);
@@ -55,6 +71,27 @@ export default defineComponent({
     height: 100%;
     display: flex;
     flex-direction: row;
+  }
+  .menu {
+    display: none;
+  }
+}
+
+@media (max-width: 767px) {
+  .wrapper {
+    .right {
+      width: 100%;
+      .side {
+        display: none;
+      }
+    }
+    .menu {
+      display: block;
+      position: fixed;
+      right: 20px;
+      top: 20px;
+      z-index: 2000;
+    }
   }
 }
 </style>
